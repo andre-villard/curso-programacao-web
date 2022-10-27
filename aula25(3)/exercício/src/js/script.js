4d5aafeec3b8c5c5178f791a958635bdc5e0ab41
André Villard de Sousa <andrevillard413@gmail.com.br>
2022-10-27 20:19:53 -0300

    comitando verificador de imagem

--- aula25(3)/exercício/src/js/script.js
+++ aula25(3)/exercício/src/js/script.js
@@ -1,1 +1,30 @@
-let box = 
+function carregar(){
+    let mensagem = document.getElementById("container-msg");
+    let imagem = document.getElementById("img")
+
+    let data = new Date();
+    //  let hora = data.getHours();
+      let minutos = data.getMinutes();
+     let main = document.getElementById("main");
+     let hora = 19;
+
+    mensagem.innerHTML = `Agora são ${hora} : ${minutos}`;
+
+    if (hora > 0 && hora <= 12){
+        imagem.src = "./assets/img/manha.jpg";
+        document.body.style.background = "lightblue";
+        main.style.background = "white"
+        mensagem.style.color = "lightblue"
+       
+    } else if (hora > 12 && hora <= 18){
+        imagem.src = "./assets/img/tarde.jpg";
+        document.body.style.background = "orange";
+        main.style.background = "white"
+        mensagem.style.color = "orange"
+    } else if (hora > 18){
+        imagem.src = "./assets/img/noite.jpg";
+        document.body.style.background = "black";
+        main.style.background = "white"
+        mensagem.style.color = "black"
+    }
+}
---
+++ aula26/modelo/src/js/script.js
@@ -0,0 +1,2 @@
+let box = document.querySelector(".box, .box02")
+console.log(box)
