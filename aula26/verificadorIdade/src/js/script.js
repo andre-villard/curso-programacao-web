935d17dfbb70007653e2bc2ba6c9f0698b98bfe7
André Villard de Sousa <andrevillard413@gmail.com.br>
2022-11-02 16:37:07 -0300

    adicionando

---
+++ aula26/verificadorIdade/src/js/script.js
@@ -0,0 +1,18 @@
+const verificar = () => {
+    let data = new Date();
+    let ano = data.getFullYear();
+    let txtAno = document.getElementById("txtano");
+    let res = document.querySelector("#res")
+    
+    if (txtAno.value.length === 0 || txtAno.value > ano) {
+        window.alert ("[ERRO BIXO BURRO] NÃO DA PRA NASCER NO FUTURO, TENTA DE NOVO")
+    } else {
+        let sexo = document.getElementsByName("sexo")
+        let idade;
+        let genero;
+        let img;
+    } if (sexo == "masc" && idade <= 1){
+        img = 
+    }
+    
+}
