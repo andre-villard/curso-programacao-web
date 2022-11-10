d5eade0cabb6fd22ffb542fb983ddd951ee1412f
André Villard de Sousa <andrevillard413@gmail.com.br>
2022-11-10 20:10:20 -0300

    cimitand 27

--- aula27/JogoDaVelha/scripts/script.js
+++ aula27/JogoDaVelha/scripts/script.js
@@ -23,15 +23,15 @@ window.addEventListener("DOMContentLoaded", () => {
     */
 
     const winningConditions = [
-        [0][1][2],
-        [3][4][5],
-        [6][7][8],
-        [0][3][6],
-        [1][4][7],
-        [2][5][8],
-        [0][4][8],
-        [2][4][6],
-    ]
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
 
     const isValidAction = tile => {
         if(tile.innerText === "X" || tile.innerText === "O" ){
@@ -113,7 +113,7 @@ tiles.forEach((tile, index) => {
 });
 
 const resetBoard = () => {
-    board = ["","","","","","","","","",];
+    board = ["","","","","","","","",""];
 
     isGameActive = true;
     announcer.classList.add("hide");
